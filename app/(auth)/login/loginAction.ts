82f00fc7e4d571bf3157836c5556bd6849da04cf
hisham <hishamduke@gmail.com>
2024-04-11 14:30:50 +0530

    Better error handling in login page

--- app/(auth)/login/loginAction.ts
+++ app/(auth)/login/loginAction.ts
@@ -3,13 +3,14 @@
 import { ofetcher } from "@/lib/ofetcher";
 import { formDataToObject } from "@/lib/utils";
 import { cookies } from "next/headers";
-import { z } from "zod";
+import { FetchError } from "ofetch";
+import { ZodError, z } from "zod";
 
 export async function loginAction(formData: FormData) {
   let errorMessage = "";
   const schema = z.object({
     email: z.string().email().toLowerCase(),
-    password: z.string().min(6),
+    password: z.string().min(6, "Password must contain minimun of 6 letters"),
   });
 
   try {
@@ -25,11 +26,17 @@ export async function loginAction(formData: FormData) {
         if (typeof error == "string") {
           errorMessage = error;
         }
+      } else {
         errorMessage = "We could not process your request";
       }
     }
   } catch (error) {
-    console.log("ERROR", JSON.stringify(error));
+    if (error instanceof ZodError) {
+      if (error.errors[0].message) {
+        errorMessage = error.errors[0]?.message;
+      }
+    }
+    console.log(error);
   } finally {
     return errorMessage;
   }
